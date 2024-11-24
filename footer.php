7b66d258fb2570da88a6afad45b50cfb8dcc3142
Aspire5738z <gonzalez.cristhian0136@gmail.com>
2024-11-24 15:57:36 -0300

    Tarea Semana 6 Dia 5

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- SCRIPTS -->
+<script src="http://localhost/tallerpoophp-base/js/jquery.min.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/popper.min.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/bootstrap.min.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/sweetalert2.all.min.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/select2.min.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/all.min.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/moment.min.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/windowcenter.js"></script>
+<script src="http://localhost/tallerpoophp-base/js/index.js"></script>
---
+++ header.php
@@ -0,0 +1,11 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- BOOTSTRAP 4 -->
+<link rel="stylesheet" href="http://localhost/tallerpoophp-base/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://localhost/tallerpoophp-base/css/all.min.css">
+<link rel="stylesheet" href="http://localhost/tallerpoophp-base/css/sweetalert2.min.css">
+<link rel="stylesheet" href="http://localhost/tallerpoophp-base/css/select2.min.css">
+<link rel="stylesheet" href="http://localhost/tallerpoophp-base/css/index.css">
+
+<!-- <link rel="stylesheet" href="/tallerpoophp/css/mystyle.css"> -->
